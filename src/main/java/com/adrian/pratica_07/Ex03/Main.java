e0afcfb17f869bff59cb01b99f6683bc3db8d947
Adrian Silva <adrians@alunos.utfpr.edu.br>
2022-04-27 19:59:57 -0300

    Ex03

--- src/main/java/com/adrian/pratica_07/Ex03/Main.java
+++ src/main/java/com/adrian/pratica_07/Ex03/Main.java
@@ -2,9 +2,17 @@ package com.adrian.pratica_07.Ex03;
 
 public class Main {
     public static void main(String[] args) {
-        Gerente g1 = new Gerente("joao", "123", 12000, 3000);
+        // Gerente g1 = new Gerente("joao", "123", 12000, 3000);
+        // Autenticador autenticador = new Autenticador();
+        // if (autenticador.autenticar(g1, false)) {
+        // System.out.println("Usuário autenticado!");
+        // } else {
+        // System.out.println("Login ou password incorretos!");
+        // }
+
+        Gerente g1 = new Gerente("joao", "aaa", 12000, 3000);
         Autenticador autenticador = new Autenticador();
-        if (autenticador.autenticar(g1, false)) {
+        if (autenticador.autenticar(g1, true)) {
             System.out.println("Usuário autenticado!");
         } else {
             System.out.println("Login ou password incorretos!");
