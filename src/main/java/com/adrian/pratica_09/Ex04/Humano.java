ca3e5871a302c3f2626ceb35b35c6971d219ce4c
Adrian Silva <88066498+Adri4nSilva@users.noreply.github.com>
2022-05-18 17:19:11 -0300

    pratica 09

--- src/main/java/com/adrian/pratica_09/Ex04/Humano.java
+++ src/main/java/com/adrian/pratica_09/Ex04/Humano.java
@@ -1,7 +1,6 @@
 package com.adrian.pratica_09.Ex04;
 
 public class Humano implements CapazDeAssobiar{
-
     @Override
     public void assobiar(String assobio) {
         System.out.println(assobio);
