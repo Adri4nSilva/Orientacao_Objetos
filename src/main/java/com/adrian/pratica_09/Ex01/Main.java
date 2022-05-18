013ba87cc2171a8966d7976b1c07e5f54470bff2
Adrian Silva <adrians@alunos.utfpr.edu.br>
2022-05-18 17:09:58 -0300

    pratica 09

--- src/main/java/com/adrian/pratica_09/Ex01/Main.java
+++ src/main/java/com/adrian/pratica_09/Ex01/Main.java
@@ -2,11 +2,19 @@ package com.adrian.pratica_09.Ex01;
 
 public class Main {
     public static void main(String[] args) {
-        String minhaFrase = "Aluno";
+        // String minhaFrase = "Aluno";
+        // MinhaInterface myInterface = new MinhaClasse();
+        // myInterface.setFrase(minhaFrase);
+        // myInterface.concatenar(" de Programação OO");
+        // int tam = myInterface.tamanho();
+        // System.out.printf("Frase: %s, Tam: %d", myInterface.getFrase(), tam);
+
+        String minhaFrase = "";
         MinhaInterface myInterface = new MinhaClasse();
         myInterface.setFrase(minhaFrase);
-        myInterface.concatenar(" de Programação OO");
+        myInterface.concatenar("Aluno de Programação");
+        myInterface.concatenar(" Java");
         int tam = myInterface.tamanho();
-        System.out.printf("Frase: %s, Tam: %d", myInterface.getFrase(), tam);
+        System.out.printf("Frase: %s, Tam: %d", myInterface.getFrase(), myInterface.tamanho());
     }
 }
